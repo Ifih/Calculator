43ccc4ed757b3098097bc9b6ce81ee7922daa953
Ifih <142207575+Ifih@users.noreply.github.com>
2025-07-18 16:47:52 +0100

    Added calculator.py

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+def calculator():
+ print("Simple Calculator")
+ print("1. ➕ Addition")
+ print("2. ➖ Subtraction")
+ print("3. ✖️ Multiplication")
+ print("4. ➗ Division")
+
+ choice = input("Enter your choice (1/2/3/4): ")
+
+ if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter first number 🙂: "))
+    num2 = float(input("Enter second number 🙂: "))
+
+ if choice == '1':
+    print(num1, "+", num2, "=", num1 + num2)
+
+ elif choice == '2':
+    print(num1, "-", num2, "=", num1 - num2)
+
+ elif choice == '3':
+    print(num1, "*", num2, "=", num1 * num2)
+
+ elif choice == '4':
+    if num2 != 0:
+        print(num1, "/", num2, "=", num1 / num2)
+    else:
+        print("Error! Division by zero is not allowed.😠")
+ else:
+
+    print("Invalid input☹️")
+
+calculator()
